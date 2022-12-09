96fa8fd9d6ebd8da9506e2fff2faff9c643f54d4
glinkinzhenya <glinkinzhenya@gmail.com>
2022-12-09 12:50:04 +0200

    1

---
+++ script.js
@@ -0,0 +1,33 @@
+// Написать функцию assignObjects(obj1, obj2), которая принимает аргументами 2 объекта и возвращает новый,
+//     который состоит из свойств обоих объектов(склеить).Если свойство повторяется, то взять значение второго объекта
+
+// assignObjects({ x: 10, y: 20 }, { z: 30 }) вывод { x: 10, y: 20, z: 30 }
+// assignObjects({ x: 10 }, { x: 20, y: 30 }) вывод { x: 20, y: 30 }
+
+const tra = {
+    x: 20,
+    y: 20,
+    z: 60
+};
+
+const tru = {
+    x: 10,
+    y: 20,
+    z: 30
+};
+
+const newObj = {};
+
+function assignObjects(obj1, obj2) {
+    for (let key in obj1) {
+        newObj[key] = obj1[key];
+    }
+
+    for (let key in obj2) {
+        newObj[key] = obj2[key];
+    }
+
+    return newObj;
+};
+
+console.log(assignObjects(tra, tru));
